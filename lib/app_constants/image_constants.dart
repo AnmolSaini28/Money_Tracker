275a270c639e1562fe507c6d3e42b36da6ae0847
AnmolSaini28 <sainianmol1507@gmail.com>
2024-07-08 22:37:29 +0530

    Add categories

---
+++ lib/app_constants/image_constants.dart
@@ -0,0 +1,28 @@
+class ImageConstants {
+  static const BASE_IMG_PATH = "assets/icons/cat/";
+
+  static const IMG_PATH_COFFEE = "${BASE_IMG_PATH}coffee.png";
+  static const IMG_PATH_FAST_FOOD = "${BASE_IMG_PATH}fast-food.png";
+  static const IMG_PATH_GIFT_BOX = "${BASE_IMG_PATH}gift-box.png";
+  static const IMG_PATH_FOOD = "${BASE_IMG_PATH}hot-pot.png";
+  static const IMG_PATH_SHIRT = "${BASE_IMG_PATH}hawaiian-shirt.png";
+  static const IMG_PATH_MOVIE = "${BASE_IMG_PATH}movie.png";
+  static const IMG_PATH_MAKEUP = "${BASE_IMG_PATH}makeup-pouch.png";
+  static const IMG_PATH_MOBILE = "${BASE_IMG_PATH}mobile-transfer.png";
+  static const IMG_PATH_MUSIC = "${BASE_IMG_PATH}music.png";
+  static const IMG_PATH_POPCORN = "${BASE_IMG_PATH}popcorn.png";
+  static const IMG_PATH_RESTAURANT = "${BASE_IMG_PATH}restaurant.png";
+  static const IMG_PATH_SHOPPING = "${BASE_IMG_PATH}shopping-cart.png";
+  static const IMG_PATH_PETROL = "${BASE_IMG_PATH}petrol-pump.png";
+  static const IMG_PATH_SNACK = "${BASE_IMG_PATH}snack.png";
+  static const IMG_PATH_TOOLS = "${BASE_IMG_PATH}tools.png";
+  static const IMG_PATH_TRAVEL = "${BASE_IMG_PATH}travel.png";
+  static const IMG_PATH_VEGETABLES = "${BASE_IMG_PATH}vegetables.png";
+  static const IMG_PATH_VEHICLE = "${BASE_IMG_PATH}vehicles.png";
+  static const IMG_PATH_WATCH = "${BASE_IMG_PATH}watch.png";
+  static const IMG_PATH_HOME = "${BASE_IMG_PATH}home.png";
+  static const IMG_PATH_HEALTH = "${BASE_IMG_PATH}health.png";
+  static const IMG_PATH_SPORTS = "${BASE_IMG_PATH}sports.png";
+  static const IMG_PATH_EDUCATION = "${BASE_IMG_PATH}education.png";
+  static const IMG_PATH_GYM = "${BASE_IMG_PATH}gym.png";
+}
