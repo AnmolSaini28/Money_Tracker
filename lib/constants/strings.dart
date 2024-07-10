f9909c51370948837dcdd3de8d2b25cc9172d659
AnmolSaini28 <sainianmol1507@gmail.com>
2024-07-10 12:04:11 +0530

    Upload App constants

---
+++ lib/constants/strings.dart
@@ -0,0 +1,71 @@
+//database
+String databaseName = 'dayManagerDatabase';
+String transactionTable = 'transaction_table';
+
+//department name string
+String coffee = 'Coffee';
+String lottery = 'Lottery';
+String education = 'Education';
+String fastFood = 'Fast-Food';
+String gift = 'Gift-Box';
+String gym = 'Gym';
+String clothing = 'Clothing';
+String health = 'Health';
+String home = 'Home';
+String food = 'Food';
+String beauty = 'Beauty';
+String Recharge = 'Recharge';
+String bills = 'Bills';
+String movie = 'Movie';
+String entertainment = 'Entertainment';
+String petrol = 'Petrol';
+String investment = 'Investment';
+String restaurant = 'Restaurant';
+String shopping = 'Shopping';
+String snacks = 'Snacks';
+String sports = 'Sports';
+String tools = 'Tools';
+String travel = 'Travel';
+String groceries = 'Groceries';
+String transport = 'Transport';
+String salary = 'Salary';
+String accessories = 'Accessories';
+String others = 'Others';
+
+//png path in assets
+String pngPath(String pngName) => "assets/icons/cat/$pngName.png";
+
+//department png name string
+String coffeePng = 'coffee';
+String lotteryPng = 'dice';
+String educationPng = 'education';
+String fastFoodPng = 'fast-food';
+String giftPng = 'gift-box';
+String gymPng = 'gym';
+String clothingPng = 'hawaiian-shirt';
+String healthPng = 'health';
+String homePng = 'home';
+String foodPng = 'hot-pot';
+String beautyPng = 'makeup-pouch';
+String RechargePng = 'mobile-transfer';
+String billsPng = 'mobile-transfer';
+String moviePng = 'movie';
+String entertainmentPng = 'music';
+String petrolPng = 'petrol-pump';
+String investmentPng = 'piggy-bank';
+String restaurantPng = 'restaurant';
+String shoppingPng = 'shopping-cart';
+String snacksPng = 'snack';
+String sportsPng = 'sports';
+String toolsPng = 'tools';
+String travelPng = 'travel';
+String groceriesPng = 'vegetables';
+String transportPng = 'vehicles';
+String salaryPng = 'wallet';
+String accessoriesPng = 'watch';
+String othersPng = 'dice';
+
+// methods
+String fullReport = "All";
+String income = "Income";
+String expense = "Expense";
